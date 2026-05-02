d94c0bc616585673cdc96c183ec07d5f88b282ea
christerac12 <christerac12@gmail.com>
2026-05-02 00:32:45 -0600

    Refactor variables.tf to define variables

--- modules/production/variables.tf
+++ modules/production/variables.tf
@@ -1,35 +1,15 @@
-resource "aws_security_group" "public_sg" {
-  name   = "public_sg"
-  vpc_id = aws_vpc.vpc.id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # replace with your ip
-  }
+variable "vpc_id" {
+  type        = string
 }
 
-resource "aws_security_group" "private_sg" {
-  name   = "private_sg"
-  vpc_id = aws_vpc.vpc.id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+variable "public_subnet_id" {
+  type        = string
+}
 
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.public_sg.id]
-  }
+variable "private_subnet_id" {
+  type        = string
+}
+
+variable "key_name" {
+  type        = string
 }
