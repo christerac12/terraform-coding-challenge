e9719a5f9f57be0c5eabc1a25735072d4afea019
christerac12 <christerac12@gmail.com>
2026-05-02 00:30:32 -0600

    Add Terraform variables for VPC and AWS configuration

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  sensitive   = true
+}
