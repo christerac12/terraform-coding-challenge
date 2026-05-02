9878ea3562cee58dfdee003bba1f545fccff5564
christerac12 <christerac12@gmail.com>
2026-05-02 00:32:13 -0600

    Refactor Terraform resources for key pair and file
    Updated local_file resource to use dynamic filename and added file permissions. Changed key_name in aws_key_pair resource.

--- main.tf
+++ main.tf
@@ -1,71 +1,16 @@
 # Assume you have Admin Access on the AWS Account
 
-
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.0.0/24"
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_eip" "eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.private_subnet.id
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "private_rta" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "public_rta" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
 resource "tls_private_key" "kp" {
   algorithm = "RSA"
 }
 
 resource "local_file" "kp_private" {
-  content  = tls_private_key.kp.private_key_pem
-  filename = "./kp.pem"
+  content         = tls_private_key.kp.private_key_pem
+  filename        = "${path.module}/kp.pem"
+  file_permission = "0600"
 }
 
 resource "aws_key_pair" "kp_public" {
-  key_name   = "kp.pem"
+  key_name   = "terraform-kp"
   public_key = tls_private_key.kp.public_key_openssh
 }
-
-
