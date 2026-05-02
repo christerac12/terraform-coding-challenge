7dee28e22439770fb6ad6bd46640f91e0c3dd048
christerac12 <christerac12@gmail.com>
2026-05-02 00:59:11 -0600

    Fixing workflows to use repo secrets rather than variables in terraform.tfvars that were used for testing

--- variables.tf
+++ variables.tf
@@ -18,12 +18,4 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_access_key" {
-  type      = string
-  sensitive = true
-}
 
-variable "aws_secret_key" {
-  type      = string
-  sensitive = true
-}
